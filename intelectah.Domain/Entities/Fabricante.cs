3e42fe7e6c4be182a3a59b6679cfacf1e45198da
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-18 19:08:59 -0300

    refactor: Add validate domain

--- intelectah.Domain/Entities/Fabricante.cs
+++ intelectah.Domain/Entities/Fabricante.cs
@@ -1,14 +1,16 @@
-﻿namespace intelectah.Domain.Entities
+﻿using intelectah.Domain.Validation;
+
+namespace intelectah.Domain.Entities
 {
     public class Fabricante : BaseEntity
     {
-        public Fabricante(string nome, string paisOrigem, int anoFundacao, string website, ICollection<Veiculo> veiculos)
+        public Fabricante(string nome, string paisOrigem, int anoFundacao, string website)
         {
+            ValidateDomain(nome, paisOrigem, anoFundacao, website);
             Nome = nome;
             PaisOrigem = paisOrigem;
             AnoFundacao = anoFundacao;
             Website = website;
-            Veiculos = veiculos;
         }
 
         public string Nome { get; private set; }
@@ -17,5 +19,22 @@
         public string Website { get; private set; }
 
         public ICollection<Veiculo> Veiculos { get; set; }
+
+        public void Update(string nome, string paisOrigem, int anoFundacao, string website)
+        {
+            ValidateDomain(nome, paisOrigem, anoFundacao, website);
+            Nome = nome;
+            PaisOrigem = paisOrigem;
+            AnoFundacao = anoFundacao;
+            Website = website;
+        }
+
+        private static void ValidateDomain(string nome, string paisOrigem, int anoFundacao, string website)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "O nome é obrigatório");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(paisOrigem), "O pais de origem é obrigatório");
+            DomainExceptionValidation.When(anoFundacao <= 0, "O ano de fundação deve ser maior que zero");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(website), "O website é obrigatório");
+        }
     }
 }
