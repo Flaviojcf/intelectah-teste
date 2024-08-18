2fa780d067e7457b07f899cbe0516c78057eecdb
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-18 15:28:35 -0300

    Add project files.

---
+++ intelectah.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace intelectah.Domain.Entities
+{
+    public class BaseEntity
+    {
+        protected BaseEntity() { }
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; private set; }
+        public bool IsActive { get; private set; }
+    }
+}
