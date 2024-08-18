b26e518e870a6c221da504cb600e9944104eab8b
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-18 18:35:00 -0300

    feat: Create concessionaria unit test

---
+++ intelectah.Tests/Domain/ConcessionariaUnitTest.cs
@@ -0,0 +1,175 @@
+﻿using intelectah.Domain.Entities;
+using intelectah.Domain.Validation;
+
+namespace intelectah.Tests.Domain
+{
+    [Collection(nameof(Concessionaria))]
+    public class ConcessionariaUnitTest
+    {
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithValidData_ReturnsConcessionaria()
+        {
+            var concessionaria = new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10);
+
+            Assert.Equal("Concessionaria Name", concessionaria.Nome);
+            Assert.Equal("Concessionaria Address", concessionaria.Endereco);
+            Assert.Equal("Concessionaria City", concessionaria.Cidade);
+            Assert.Equal("Concessionaria State", concessionaria.Estado);
+            Assert.Equal("12345678", concessionaria.CEP);
+            Assert.Equal("11987654321", concessionaria.Telefone);
+            Assert.Equal("client@example.com", concessionaria.Email);
+            Assert.Equal(10, concessionaria.CapacidadeMaximaVeiculos);
+            Assert.True(concessionaria.IsActive);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyName_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O nome é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyEndereco_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O endereço é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyCidade_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("A cidade é obrigatória", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyEstado_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O estado é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyCEP_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O CEP é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyTelefone_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "", "client@example.com", 10));
+
+            Assert.Equal("O telefone é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithEmptyEmail_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "", 10));
+
+            Assert.Equal("O e-mail é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Create Concessionaria")]
+        public void CreateConcessionaria_WithZeroCapacidadeMaxima_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 0));
+
+            Assert.Equal("A capacidade máxima de veículos deve ser maior que zero", exception.Message);
+        }
+
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyEndereco_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O endereço é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyCidade_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("A cidade é obrigatória", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyEstado_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "", "12345678", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O estado é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyCEP_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "", "11987654321", "client@example.com", 10));
+
+            Assert.Equal("O CEP é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyTelefone_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "", "client@example.com", 10));
+
+            Assert.Equal("O telefone é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithEmptyEmail_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "", 10));
+
+            Assert.Equal("O e-mail é obrigatório", exception.Message);
+        }
+
+        [Fact]
+        [Trait("Domain", "Update Concessionaria")]
+        public void UpdateConcessionaria_WithZeroCapacidadeMaxima_ThrowsDomainExceptionValidation()
+        {
+            var exception = Assert.Throws<DomainExceptionValidation>(() => new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 0));
+
+            Assert.Equal("A capacidade máxima de veículos deve ser maior que zero", exception.Message);
+        }
+
+
+        [Fact]
+        [Trait("Domain", "DeActive Concessionaria")]
+        public void DeActive_WhenCalled_SetsIsActiveToFalseAndUpdateDate()
+        {
+            var concessionaria = new Concessionaria("Concessionaria Name", "Concessionaria Address", "Concessionaria City", "Concessionaria State", "12345678", "11987654321", "client@example.com", 10);
+
+            concessionaria.DeActive();
+
+            Assert.False(concessionaria.IsActive);
+            Assert.True(concessionaria.UpdatedAt > DateTime.Now.AddSeconds(-1));
+        }
+    }
+}
