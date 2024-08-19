43e444f003ff5c6bbec238e8f087f6d5cb742a90
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-19 17:13:45 -0300

    feat: Create concessionarias queries and handler

---
+++ intelectah.Application/Queries/ConcessionariaQueries/GetAllConcessionarias/GetAllConcessionariasQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using intelectah.Domain.Entities;
+using intelectah.Domain.Repositories;
+using MediatR;
+
+namespace intelectah.Application.Queries.ConcessionariaQueries.GetAllConcessionarias
+{
+    public class GetAllConcessionariasQueryHandler(IConcessionariaRepository concessionariaRepository) : IRequestHandler<GetAllConcessionariasQuery, List<Concessionaria>>
+    {
+        private readonly IConcessionariaRepository _concessionariaRepository = concessionariaRepository;
+        public async Task<List<Concessionaria>> Handle(GetAllConcessionariasQuery request, CancellationToken cancellationToken)
+        {
+            return await _concessionariaRepository.GetAllAsync();
+        }
+    }
+}
---
+++ intelectah.Application/Queries/ConcessionariaQueries/GetConcessionariaById/GetConcessionariaByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using intelectah.Domain.Entities;
+using intelectah.Domain.Repositories;
+using MediatR;
+
+namespace intelectah.Application.Queries.ConcessionariaQueries.GetConcessionariaById
+{
+    public class GetConcessionariaByIdQueryHandler(IConcessionariaRepository concessionariaRepository) : IRequestHandler<GetConcessionariaByIdQuery, Concessionaria>
+    {
+        private readonly IConcessionariaRepository _concessionariaRepository = concessionariaRepository;
+
+
+        public async Task<Concessionaria> Handle(GetConcessionariaByIdQuery request, CancellationToken cancellationToken)
+        {
+            var concessionaria = await _concessionariaRepository.GetByIdAsync(request.Id);
+
+            return concessionaria;
+        }
+    }
+}
+
