2c8e804506de91a68fe32d71de8671c6c0ff16f7
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-19 23:52:10 -0300

    feat: Create usuario command and handler

---
+++ intelectah.Application/Commands/UsuarioCommands/CreateUsuarioCommand.cs
@@ -0,0 +1,13 @@
+﻿using intelectah.Domain.TypesEnum;
+using MediatR;
+
+namespace intelectah.Application.Commands.UsuarioCommands
+{
+    public class CreateUsuarioCommand(string nome, string senha, string email, NivelAcesso nivelAcesso) : IRequest<int>
+    {
+        public string Nome { get; set; } = nome;
+        public string Senha { get; set; } = senha;
+        public string Email { get; set; } = email;
+        public NivelAcesso NivelAcesso { get; set; } = nivelAcesso;
+    }
+}
