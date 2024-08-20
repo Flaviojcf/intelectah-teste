ea9efba00890dbf7c79bbf13a11b408bf4d3fd1e
flavio <flaviojcostafilho@gmail.com>
2024-08-19 23:03:19 -0300

    feat: Create query by name

---
+++ intelectah.Application/Queries/FabricanteQueries/GetFabricanteByName/GetFabricanteByNameQuery.cs
@@ -0,0 +1,10 @@
+﻿using intelectah.Domain.Entities;
+using MediatR;
+
+namespace intelectah.Application.Queries.FabricanteQueries.GetFabricanteByName
+{
+    public class GetFabricanteByNameQuery(string nome) : IRequest<Fabricante>
+    {
+        public string Nome { get; set; } = nome;
+    }
+}
