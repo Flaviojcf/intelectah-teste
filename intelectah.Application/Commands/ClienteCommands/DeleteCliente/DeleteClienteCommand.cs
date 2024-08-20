47459a0ab9ec35329caa3671631a362545ee0574
flavio <flaviojcostafilho@gmail.com>
2024-08-19 22:29:43 -0300

    feat: Create cliente commands

---
+++ intelectah.Application/Commands/ClienteCommands/DeleteCliente/DeleteClienteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace intelectah.Application.Commands.ClienteCommands.DeleteCliente
+{
+    public class DeleteClienteCommand(int id) : IRequest<Unit>
+    {
+        public int Id { get; set; } = id;
+    }
+}
