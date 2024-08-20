17a42f491f396f28e3151b8fb663345e71bef1be
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-20 02:58:23 -0300

    feat: Add repositories

---
+++ intelectah.Infrastructure/Persistance/Repositories/VeiculoRepository.cs
@@ -0,0 +1,28 @@
+﻿using intelectah.Domain.Entities;
+using intelectah.Domain.Repositories;
+
+namespace intelectah.Infrastructure.Persistance.Repositories
+{
+    public class VeiculoRepository : IVeiculoRepository
+    {
+        public Task CreateAsync(Veiculo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Veiculo>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Veiculo> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
