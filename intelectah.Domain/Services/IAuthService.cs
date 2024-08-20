2b4ba4135404e7eded42142254feb8b5bc53a94f
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-19 23:53:13 -0300

    feat: Add auth interface service

---
+++ intelectah.Domain/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using intelectah.Domain.TypesEnum;
+
+namespace intelectah.Domain.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJwtToken(string email, NivelAcesso nivelAcesso);
+        string ComputeSha256Hash(string password);
+    }
+}
