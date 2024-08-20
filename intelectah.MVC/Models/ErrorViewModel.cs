226b70da47033adfea40d94af76af7731f017d63
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-20 00:25:10 -0300

    feat: Add Mvc

---
+++ intelectah.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace intelectah.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
