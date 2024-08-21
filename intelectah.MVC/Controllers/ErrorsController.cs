e37b0ebbd7f8e4d068ea8f5056b21082c15d1d12
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-21 00:11:07 -0300

    feat: Add Error route

---
+++ intelectah.MVC/Controllers/ErrorsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace intelectah.MVC.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [Route("Error404")]
+        public IActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
