079580ee219b3ec4adcbac7fa86e99796bdce435
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-26 15:45:36 -0300

    feat: Add new exception

---
+++ intelectah.Domain/Exceptions/FabricanteAlreadyExistException.cs
@@ -0,0 +1,8 @@
+﻿namespace intelectah.Domain.Exceptions
+{
+    public class FabricanteAlreadyExistException : Exception
+    {
+        public FabricanteAlreadyExistException(string nome) : base($"O fabricante com o nome '{nome}' já existe.") { }
+
+    }
+}
