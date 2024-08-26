adf91637ac3c5e0cc0f973f342f198f77cffe0ec
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-26 12:12:31 -0300

    refactor: Add role type

--- intelectah.Application/OutPutModels/LoginUsuarioOutputModel.cs
+++ intelectah.Application/OutPutModels/LoginUsuarioOutputModel.cs
@@ -1,8 +1,11 @@
-﻿namespace intelectah.Application.OutPutModels
+﻿using intelectah.Domain.TypesEnum;
+
+namespace intelectah.Application.OutPutModels
 {
-    public class LoginUsuarioOutputModel(string email, string token)
+    public class LoginUsuarioOutputModel(string email, string token, NivelAcesso nivelAcesso)
     {
         public string Email { get; private set; } = email;
         public string Token { get; private set; } = token;
+        public NivelAcesso NivelAcesso { get; private set; } = nivelAcesso;
     }
 }
