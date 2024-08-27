b6ec3990db41de3499735a221f487bc4bc142180
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-27 12:04:00 -0300

    feat: Create vendas structure

--- intelectah.MVC/Controllers/VendaController.cs
+++ intelectah.MVC/Controllers/VendaController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace intelectah.MVC.Controllers
 {
+    [Authorize(Roles = "Vendedor")]
     public class VendaController : Controller
     {
+        private readonly IMediator _mediator;
+
+
+        public VendaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
         public IActionResult Vendas()
         {
             return View();
