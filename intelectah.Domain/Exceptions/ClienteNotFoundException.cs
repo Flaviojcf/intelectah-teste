f99593935be4ff1e67d5379a4545c48e21cbf6da
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-27 10:58:51 -0300

    feat: Add new exception

---
+++ intelectah.Domain/Exceptions/ClienteNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace intelectah.Domain.Exceptions
+{
+    public class ClienteNotFoundException : Exception
+    {
+        public ClienteNotFoundException(int id) : base($"O cliente com o id '{id}' não foi encontrado.") { }
+
+    }
+}
