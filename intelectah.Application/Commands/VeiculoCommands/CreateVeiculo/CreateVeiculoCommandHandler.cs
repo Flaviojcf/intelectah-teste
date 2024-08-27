7f1eebf8736c249afa38d1f49530d231b6adc97b
Flávio Costa <flaviojcostafilho@gmail.com>
2024-08-27 08:46:13 -0300

    feat: Add veiculo validate service

--- intelectah.Application/Commands/VeiculoCommands/CreateVeiculo/CreateVeiculoCommandHandler.cs
+++ intelectah.Application/Commands/VeiculoCommands/CreateVeiculo/CreateVeiculoCommandHandler.cs
@@ -1,14 +1,18 @@
-﻿using intelectah.Domain.Entities;
+﻿using intelectah.Application.Services.Interfaces;
+using intelectah.Domain.Entities;
 using intelectah.Domain.Repositories;
 using MediatR;
 
 namespace intelectah.Application.Commands.VeiculoCommands.CreateVeiculo
 {
-    public class CreateVeiculoCommandHandler(IVeiculoRepository veiculoRepository) : IRequestHandler<CreateVeiculoCommand, int>
+    public class CreateVeiculoCommandHandler(IVeiculoRepository veiculoRepository, IValidateVeiculoRulesService validateVeiculoRulesService) : IRequestHandler<CreateVeiculoCommand, int>
     {
         private readonly IVeiculoRepository _veiculoRepository = veiculoRepository;
+        private readonly IValidateVeiculoRulesService _validateVeiculoRulesService = validateVeiculoRulesService;
         public async Task<int> Handle(CreateVeiculoCommand request, CancellationToken cancellationToken)
         {
+            _validateVeiculoRulesService.ValidateVeiculo(request);
+
             var veiculo = new Veiculo(request.Modelo, request.AnoFabricacao, request.Preco, request.TipoVeiculo, request.Descricao, request.FabricanteID);
 
             await _veiculoRepository.CreateAsync(veiculo);
---
+++ intelectah.Application/Services/Interfaces/IValidateVeiculoRulesService.cs
@@ -0,0 +1,12 @@
+﻿using intelectah.Application.Commands.VeiculoCommands.CreateVeiculo;
+using intelectah.Application.Commands.VeiculoCommands.UpdateVeiculo;
+
+namespace intelectah.Application.Services.Interfaces
+{
+
+    public interface IValidateVeiculoRulesService
+    {
+        void ValidateVeiculo(CreateVeiculoCommand command);
+        void ValidateUpdateVeiculo(UpdateVeiculoCommand command);
+    }
+}
